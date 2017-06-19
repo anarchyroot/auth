1084ee2709245d37c3432d932eebffa67ddd3559
anarchyroot <matt@shotbymatt.com>
2017-06-19 04:24:25 +0100

    created login.html

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,29 @@ from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
-from accounts.forms import UserRegistrationForm
+from accounts.forms import UserRegistrationForm, UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(email=request.POST.get('email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your email or password was not recognised")
+
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
 
 
 def register(request):
